577c5e558a6fc4f3354e2c852e2fe725375dd3b8
CSkai514 <144076888+CSkai514@users.noreply.github.com>
2025-03-02 22:26:49 +0800

    first commit

---
+++ test1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+       Here is the first php file
+    </h1>
+  <h1><?php echo rand(1000000, 10000000000 ); ?></h1>  
+
+</body>
+</html>
